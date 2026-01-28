a5513b2a09c44bba0b8fcd4598d2eb289ddd61e4
Pedrao01 <149744853+Pedrao01@users.noreply.github.com>
2026-01-28 07:50:51 -0300

    Criação da tabela relacional user_id e a tabela roles

---
+++ app/models/role.py
@@ -0,0 +1,17 @@
+from app import db
+
+user_roles = db.Table(
+    'user_roles',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+    db.Column('roles_id', db.Integer, db.ForeignKey('roles.id'), primary_key=True)
+)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<role: {self.name}>'
