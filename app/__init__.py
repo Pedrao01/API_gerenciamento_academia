3445164a70fbdb1fca8af9d00303f9a93bd621c0
Pedrao01 <149744853+Pedrao01@users.noreply.github.com>
2026-01-28 07:47:42 -0300

    Carrega o usuario logado na variavel global g

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, session, g
 from flask_sqlalchemy import SQLAlchemy
 from app.config import DevelopmentConfig
 from flask_migrate import Migrate
@@ -14,10 +14,21 @@ def create_app():
 
     Migrate(app, db)
 
-    from app import models
+    from app.models.user import User
+
+    @app.before_request
+    def load_logged_user():
+        user_id = session.get('user_id')
+        if user_id is None:
+            g.user = None
+        else:
+            g.user = User.query.get(user_id)
 
     # Blueprints
     from app.routes.auth import auth_bp
     app.register_blueprint(auth_bp, url_prefix='/user')
+
+    from app.routes.main import main_bp
+    app.register_blueprint(main_bp, url_prefix='/user')
 
     return app
